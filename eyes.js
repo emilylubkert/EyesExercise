c9ef05e2fb96568b6203b63ad419f529fdff82ee
emilylubkert <elubkert@gmail.com>
2021-12-10 10:17:24 -0700

    first commit

---
+++ eyes.js
@@ -0,0 +1,19 @@
+let img = document.getElementById("image");
+
+const balls = document.getElementsByClassName("ball");
+document.onmousemove = (event) => {
+  let x = (event.clientX * 100) / window.innerWidth + "%";
+  let y = (event.clientY * 100) / window.innerHeight + "%";
+
+  for (let i = 0; i < 2; i++) {
+    balls[i].style.left = x;
+    balls[i].style.top = y;
+    balls[i].transform = "translate(-" + x + ",-" + y + ")";
+  }
+};
+
+function moveImage(event) {
+    img.style.left = event.clientX - 150 + "px";
+    img.style.top = event.clientY - 200 + "px";
+}
+document.addEventListener('mousemove', moveImage)
